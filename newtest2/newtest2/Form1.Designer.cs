c1994f4d0ad1ced35d7bb07d87bbde697a3766a8
Ernest <ernest.jinian@gmail.com>
2018-02-18 13:48:26 -0800

    Adding all files

---
+++ newtest2/newtest2/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace newtest2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileOpenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorOpenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.effectsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.disappearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.backgroundStartupToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.drawLinesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.InitialDirectory = "c:\\users\\Echo Romeo\\Pictures";
+            // 
+            // colorDialog1
+            // 
+            this.colorDialog1.AnyColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(290, 151);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 44);
+            this.button1.TabIndex = 0;
+            this.button1.TabStop = false;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.button2.Location = new System.Drawing.Point(290, 245);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 44);
+            this.button2.TabIndex = 1;
+            this.button2.TabStop = false;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.optionsToolStripMenuItem,
+            this.effectsToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(731, 28);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileOpenToolStripMenuItem,
+            this.colorOpenToolStripMenuItem});
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(73, 24);
+            this.optionsToolStripMenuItem.Text = "Options";
+            // 
+            // fileOpenToolStripMenuItem
+            // 
+            this.fileOpenToolStripMenuItem.Name = "fileOpenToolStripMenuItem";
+            this.fileOpenToolStripMenuItem.Size = new System.Drawing.Size(160, 26);
+            this.fileOpenToolStripMenuItem.Text = "File Open";
+            this.fileOpenToolStripMenuItem.Click += new System.EventHandler(this.fileOpenToolStripMenuItem_Click);
+            // 
+            // colorOpenToolStripMenuItem
+            // 
+            this.colorOpenToolStripMenuItem.Name = "colorOpenToolStripMenuItem";
+            this.colorOpenToolStripMenuItem.Size = new System.Drawing.Size(160, 26);
+            this.colorOpenToolStripMenuItem.Text = "Color Open";
+            this.colorOpenToolStripMenuItem.Click += new System.EventHandler(this.colorOpenToolStripMenuItem_Click);
+            // 
+            // effectsToolStripMenuItem
+            // 
+            this.effectsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.disappearToolStripMenuItem,
+            this.backgroundStartupToolStripMenuItem,
+            this.drawLinesToolStripMenuItem});
+            this.effectsToolStripMenuItem.Name = "effectsToolStripMenuItem";
+            this.effectsToolStripMenuItem.Size = new System.Drawing.Size(65, 24);
+            this.effectsToolStripMenuItem.Text = "Effects";
+            // 
+            // disappearToolStripMenuItem
+            // 
+            this.disappearToolStripMenuItem.Name = "disappearToolStripMenuItem";
+            this.disappearToolStripMenuItem.Size = new System.Drawing.Size(215, 26);
+            this.disappearToolStripMenuItem.Text = "Disappear";
+            this.disappearToolStripMenuItem.Click += new System.EventHandler(this.disappearToolStripMenuItem_Click);
+            // 
+            // backgroundStartupToolStripMenuItem
+            // 
+            this.backgroundStartupToolStripMenuItem.Name = "backgroundStartupToolStripMenuItem";
+            this.backgroundStartupToolStripMenuItem.Size = new System.Drawing.Size(215, 26);
+            this.backgroundStartupToolStripMenuItem.Text = "Background Startup";
+            this.backgroundStartupToolStripMenuItem.Click += new System.EventHandler(this.backgroundStartupToolStripMenuItem_Click);
+            // 
+            // drawLinesToolStripMenuItem
+            // 
+            this.drawLinesToolStripMenuItem.Name = "drawLinesToolStripMenuItem";
+            this.drawLinesToolStripMenuItem.Size = new System.Drawing.Size(215, 26);
+            this.drawLinesToolStripMenuItem.Text = "Draw Lines";
+            this.drawLinesToolStripMenuItem.Click += new System.EventHandler(this.drawLinesToolStripMenuItem_Click);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(57, 24);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 3000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(731, 457);
+            this.ControlBox = false;
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Location = new System.Drawing.Point(300, 0);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fileOpenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem colorOpenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem effectsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem disappearToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem backgroundStartupToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem drawLinesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ newtest2/newtest2/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace newtest2
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        Color lineColor = Color.Red;
+        public Form1()
+        {
+            InitializeComponent();
+            g = CreateGraphics();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            g.Clear(BackColor);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void fileOpenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void colorOpenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ColorDialog MyDialog = new ColorDialog();
+            colorDialog1.ShowDialog();
+            menuStrip1.BackColor = colorDialog1.Color;
+            lineColor = colorDialog1.Color;
+            g.DrawLine(new Pen(lineColor), button1.Location.X + button1.Width / 2, button1.Location.Y + button1.Height, button2.Location.X + button2.Width / 2, button2.Location.Y);
+        }
+
+        private void disappearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            menuStrip1.Visible = false;
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            menuStrip1.Visible = true;
+        }
+
+        private void backgroundStartupToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Process.Start();
+        }
+
+        private void drawLinesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //between button1 and 2
+            g.DrawLine(new Pen(lineColor), button1.Location.X + button1.Width / 2, button1.Location.Y + button1.Height, button2.Location.X + button2.Width / 2, button2.Location.Y);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
